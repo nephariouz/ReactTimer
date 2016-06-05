06aec94f548b5fce2000fb14e3673205820e9d3e
Zach <zach.bay@gmail.com>
2016-06-04 21:54:56 -0700

    Add CountdownForm component and tests

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,8 +5,10 @@ var Main = (props) => {
   return (
     <div>
       <Nav/>
-      <div>
-        {props.children}
+      <div className="row">
+        <div className="column small-centered medium-6 large-4">
+          {props.children}
+        </div>
       </div>
     </div>
   );
